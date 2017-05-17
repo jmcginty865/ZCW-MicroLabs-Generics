848f104300da9d9250a85c86b15945c4b8357e66
waxdoll23 <jmcgnty23@gmail.com>
2017-05-17 18:48:22 -0400

    Finished the second part of the lab

--- src/main/java/StackArray/GenericStack.java
+++ src/main/java/StackArray/GenericStack.java
@@ -1,6 +1,7 @@
 package StackArray;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 /**
  * Expand the ArrayList implementation of stack here to use an E[] array.  Still implement push, pop, and isEmpty.
@@ -10,6 +11,37 @@ import java.util.Arrays;
 public class GenericStack<E> {
     private E[] elements;
 
+    @SuppressWarnings("Unchecked")
     public GenericStack() {
+        elements = (E[]) new Object[0];
+    }
+
+    public void push(E item) {
+        E[] secondElementArray = (E[]) new Object[elements.length + 1];
+
+        for (int i = 0; i < elements.length; i++) {
+            secondElementArray[i] = elements[i];
+        }
+
+        secondElementArray[secondElementArray.length - 1] = item;
+
+        elements = secondElementArray;
+    }
+
+    public E pop() throws IndexOutOfBoundsException {
+        E items = elements[elements.length -1];
+        E[] updatedArray = Arrays.copyOf(elements, elements.length-1);
+
+        if (elements.length < 1) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        elements = updatedArray;
+
+        return items;
+    }
+
+    public boolean isEmpty(){
+        return elements.length == 0;
     }
 }
