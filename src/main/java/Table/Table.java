8396b5a340d501597f3e63092368ea37387d8ada
waxdoll23 <jmcgnty23@gmail.com>
2017-05-17 22:13:05 -0400

    Finsihed the thrid part.

--- src/main/java/Table/Table.java
+++ src/main/java/Table/Table.java
@@ -1,6 +1,8 @@
 package Table;
 
 import java.util.ArrayList;
+import java.util.Map;
+
 
 /**
  * This class needs to manage an ArrayList of Entry objects.  It needs a get method that takes a key and returns
@@ -10,8 +12,44 @@ import java.util.ArrayList;
  * Void return on `remove`.
  */
 public class Table<K, V> {
-    private ArrayList entries;
+    private ArrayList <Entry<K,V>> entries;
 
     public Table() {
+        entries = new ArrayList<Entry<K,V>>();
+    }
+
+    public V get (K key){
+        for(int i = 0; i < entries.size(); i++){
+            if(entries.get(i).getKey().equals(key)){
+                Entry<K,V> match = entries.get(i);
+                return  match.getValue();
+            }
+        }
+        return null;
+    }
+
+    public void put(K key, V value) {
+        boolean keyIsThere = false;
+        for (int i = 0; i < entries.size(); i++) {
+            if (entries.get(i).getKey().equals(key)) {
+                entries.set(i, new Entry<K, V>(key, value));
+                keyIsThere = true;
+            }
+        }
+        if (!keyIsThere) {
+            Entry<K, V> anotherEntry = new Entry<K, V>(key, value);
+            entries.add(anotherEntry);
+        }
+    }
+
+    public void remove(K key){
+        for (int i = 0; i < entries.size(); i++){
+            Entry<K,V> recentEntry = entries.get(i);
+                if(recentEntry.getKey().equals(key)){
+                    entries.remove(i);
+                }
+        }
+
     }
 }
+
