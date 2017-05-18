4b2422d91a8e839fb33357c17b83ea8159c4de3b
waxdoll23 <jmcgnty23@gmail.com>
2017-05-18 17:12:09 -0400

    Finished the  Generics lab.

--- src/main/java/Pair/Arrays.java
+++ src/main/java/Pair/Arrays.java
@@ -1,7 +1,12 @@
 package Pair;
 
+import sun.jvm.hotspot.gc_interface.CollectedHeapName;
+
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
+
+import static javax.swing.UIManager.get;
 
 /**
  * In here you must make firstLast, which will return a pair of the first element in the array list and the last
@@ -10,7 +15,24 @@ import java.util.Collections;
  * A max method that returns the largest item in the arraylist
  * And a minmax method that returns a pair containing the largest and smallest items from the array list
 // */
-//public class Arrays {
-//    public static <___> Pair<E> firstLast(ArrayList<___> a) {
-//    }
-//}
+public class Arrays <E> {
+    public static <E extends Comparable> Pair<E> firstLast(ArrayList<E> a) {
+        return new Pair(a.get(0), a.get(a.size() - 1));
+    }
+
+    public static <E extends Comparable> E min(ArrayList<E> list){
+        Collections.sort(list);
+        return list.get(0);
+    }
+
+    public static <E extends Comparable> E max(ArrayList<E> list){
+        Collections.sort(list);
+        return list.get(list.size() - 1);
+    }
+
+
+    public static <E extends Comparable> Pair <E> minMax(ArrayList<E> list){
+        Collections.sort(list);
+        return new Pair<E>(min(list), max(list));
+    }
+}
--- src/main/java/Pair/Pair.java
+++ src/main/java/Pair/Pair.java
@@ -1,5 +1,7 @@
 package Pair;
 
+import java.util.Comparator;
+
 /**
  * You need to store two values of type `E`, set them in a constructor, and have the following methods,
  * getFirst
@@ -7,6 +9,37 @@ package Pair;
  * min -> returns the minimum of the pair
  * max -> returns the maximum of the pair
  */
-public class Pair {
+public class Pair <E extends Comparable> {
 
+    E first;
+    E second;
+
+    public Pair(E first, E second){
+        this.first = first;
+        this.second = second;
+    }
+
+    public E getFirst() {
+        return first;
+    }
+
+    public E getSecond() {
+        return second;
+    }
+
+    public E min(){
+
+        if(first.compareTo(second) < 0){
+            return first;
+        }
+        else return second;
+    }
+
+    public E max(){
+
+        if(first.compareTo(second) > 0){
+            return first;
+        }
+        else return second;
+    }
 }
